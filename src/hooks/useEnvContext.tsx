306fe7bf626c2cb359806f5113aa7e01e5985738
Andressa Bertolini <andressabertolini08@gmail.com>
2025-05-21 19:16:04 -0300

    feat: context in search component

--- src/hooks/useEnvContext.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { EnvContext } from "../context/EnvContext";
-
-export const useEnvContext = () => {
-    const context = useContext(EnvContext);
-    return context;
-}
